803fc25ed8ea0523e71911e53cc9903fb0cdf04a
Mntsnt <mntsntleliso@gmail.com>
2022-12-08 03:22:12 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - prints the size of various types
+* Return: 0 if excited properly, non-zero otherwise
+*/
+int main(void) 
+{
+printf("Size of a char: %d byte(s) , size of(char));
+printf("Size of an int: %d byte(s) , size of(int));
+printf("Size of a long int: %d byte(s) , size of(long int));
+printf("Size of a long long int: %d byte(s) , size of(long long int));
+printf("Size of a float: %d byte(s) , size of(float));
+return (0);
+}
