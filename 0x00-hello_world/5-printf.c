edc0d28f2eda64e51d7febd65aadba587b1da69a
Mntsnt <mntsntleliso@gmail.com>
2022-12-08 03:05:48 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*Using printf function*/
+/*
+ * main - prints phrasevwith correct grammar
+ * but outcome is a piece of art
+ * return 
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
