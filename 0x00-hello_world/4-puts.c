8327949e6d5bef632f75f781b6ac13f4826ef6b9
Mntsnt <mntsntleliso@gmail.com>
2022-12-15 06:45:53 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /*
  * main
  * but
  * Return 0
  */
 
-int main(void) {
+int main(void)
+{
 
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0) ;
-
 }
