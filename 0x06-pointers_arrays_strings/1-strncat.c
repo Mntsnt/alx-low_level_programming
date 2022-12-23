549684b99fa96c6ee7947586c27ca05c309c84f7
Mntsnt <mntsntleliso@gmail.com>
2022-12-23 12:08:05 -0800

    nee

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,2 +1,18 @@
-#include "main.h"                                                              /**                                                                             * _strncat - function                                                          *@dest: char                                                                   *@src: char                                                                    *@n: int                                                                       *Return: char                                                                  */                                                                                                                                                           char *_strncat(char *dest, char *src, int n)                                   {                                                                                      int i, j;                                                                                                                                                     i = 0;                                                                                                                                                        while (dest[i] != '\0')                                                        {                                                                                      i++;                                                                   }                                                                                                                                                             j = 0;                                                                         while (src[j] != '\0' && j < n)                                                {                                                                                      dest[i] = src[j];                                                              i++;                                                                           j++;                                                                   }                                                                              dest[i] = '\0';                                                                                                                                               return (d:q
+#include "main.h"
+
+char *_strncat(char *dest, char *src, int n);
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		continue;
 	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
